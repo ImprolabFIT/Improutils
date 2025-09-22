315b8acc641c09ba654132a14513c783f026f26b
David Kramný <kramnda1@fit.cvut.cz>
2025-09-22 14:13:51 +0200

    Fix test imports

--- tests/test_recognition.py
+++ tests/test_recognition.py
@@ -1,12 +1,27 @@
 import unittest
-from improutils.acquisition import *
-from improutils.visualisation import *
-from improutils.preprocessing import *
-from improutils.segmentation import *
-from improutils.recognition import *
 from pathlib import Path
 
-base_path = Path(__name__).parent.absolute() / 'tests' / 'img'
+import cv2
+
+from improutils import (
+    aspect_ratio,
+    compactness,
+    convexity,
+    find_contours,
+    load_image,
+    ocr,
+    qr_decode,
+    qr_detect,
+    qr_detect_and_decode,
+    roundness,
+    segmentation_auto_threshold,
+    segmentation_two_thresholds,
+    solidity,
+    to_hsv,
+)
+
+base_path = Path(__name__).parent.absolute() / "tests" / "img"
+
 
 class ImageFeaturesTestCase(unittest.TestCase):
     # the following commented code can be used for executing the same test
@@ -54,10 +69,11 @@ class ImageFeaturesTestCase(unittest.TestCase):
         # -----------------------
 
     """
+
     def test_form_factor(self):
         eps = 0.04
         blue = ((90, 0, 0), (135, 255, 255))
-        img = load_image('{}/test-img.png'.format(base_path))
+        img = load_image("{}/test-img.png".format(base_path))
         img = to_hsv(img)
 
         bin_im = cv2.inRange(img, blue[0], blue[1])
@@ -69,7 +85,7 @@ class ImageFeaturesTestCase(unittest.TestCase):
     def test_aspect_ratio(self):
         eps = 0.04
         red = ((150, 0, 0), (180, 255, 255))
-        img = load_image('{}/test-img.png'.format(base_path))
+        img = load_image("{}/test-img.png".format(base_path))
         img = to_hsv(img)
 
         img_bin = cv2.inRange(img, red[0], red[1])
@@ -82,7 +98,7 @@ class ImageFeaturesTestCase(unittest.TestCase):
         eps = 0.04
         red = ((150, 0, 0), (180, 255, 255))
         yellow = ((15, 0, 0), (30, 255, 255))
-        img = load_image('{}/test-img.png'.format(base_path))
+        img = load_image("{}/test-img.png".format(base_path))
         img = to_hsv(img)
 
         img_bin_square = cv2.inRange(img, red[0], red[1])
@@ -93,13 +109,13 @@ class ImageFeaturesTestCase(unittest.TestCase):
         c_square = convexity(conts_square[0])
         c_star = convexity(conts_star[0])
         self.assertLess(1.0 - c_square, eps)
-        self.assertLess( c_star, eps*10)
+        self.assertLess(c_star, eps * 10)
 
     def test_solidity(self):
         eps = 0.041
         red = ((150, 0, 0), (180, 255, 255))
         yellow = ((15, 0, 0), (30, 255, 255))
-        img = load_image('{}/test-img.png'.format(base_path))
+        img = load_image("{}/test-img.png".format(base_path))
         img = to_hsv(img)
 
         img_bin_square = cv2.inRange(img, red[0], red[1])
@@ -116,7 +132,7 @@ class ImageFeaturesTestCase(unittest.TestCase):
         eps = 0.04
         red = ((150, 0, 0), (180, 255, 255))
         yellow = ((15, 0, 0), (30, 255, 255))
-        img = load_image('{}/test-img.png'.format(base_path))
+        img = load_image("{}/test-img.png".format(base_path))
         img = to_hsv(img)
 
         img_bin_square = cv2.inRange(img, red[0], red[1])
@@ -127,76 +143,77 @@ class ImageFeaturesTestCase(unittest.TestCase):
         c_square = compactness(conts_square[0])
         c_star = compactness(conts_star[0])
         self.assertLess(1.0 - c_square, eps)
-        self.assertLess( c_star, 0.6)
+        self.assertLess(c_star, 0.6)
+
 
 class OcrTestCase(unittest.TestCase):
-
     def test_ocr(self):
-        img = load_image('{}/ocr-img.png'.format(base_path))
+        img = load_image("{}/ocr-img.png".format(base_path))
         img_bin = segmentation_two_thresholds(img, 0, 10)
 
         # run with custom config
-        text = ocr(img_bin, config='--oem 3 -c tessedit_char_whitelist=IMPROUTILSimproutils tessedit_char_whitelist=0123456789')
-        self.assertEqual(text.strip(), 'Improutils')
-    
+        text = ocr(
+            img_bin,
+            config="--oem 3 -c tessedit_char_whitelist=IMPROUTILSimproutils tessedit_char_whitelist=0123456789",
+        )
+        self.assertEqual(text.strip(), "Improutils")
+
         # run with default config
         text = ocr(img_bin)
-        self.assertEqual(text.strip(), 'Improutils')
-        
+        self.assertEqual(text.strip(), "Improutils")
+
+
 class QRTestCase(unittest.TestCase):
-
-
-    def __test_qr(self,img,is_bgr):
-         # check if detection and decoding works individually
-        detections = qr_detect(img,is_bgr=is_bgr)
+    def __test_qr(self, img, is_bgr):
+        # check if detection and decoding works individually
+        detections = qr_detect(img, is_bgr=is_bgr)
         # qr_detect return a tuple of detections and decode takes in only one detection
         if detections is not None:
-            result = qr_decode(img, detections[0],is_bgr=is_bgr)
+            result = qr_decode(img, detections[0], is_bgr=is_bgr)
             if result is not None:
-                self.assertEqual(result, 'Kazdy Fitak chce energetak!')
+                self.assertEqual(result, "Kazdy Fitak chce energetak!")
             else:
                 print("QR code detected, but not decoded.")
         else:
             print("No QR code detected.")
 
         # check if detection and decoding works as a single function in qr_detect_and_decode
-        results = qr_detect_and_decode(img,is_bgr=is_bgr)
+        results = qr_detect_and_decode(img, is_bgr=is_bgr)
         if results is not None:
-            self.assertEqual(results[0], 'Kazdy Fitak chce energetak!')
-        
-    
+            self.assertEqual(results[0], "Kazdy Fitak chce energetak!")
+
     def test_qr_bgr(self):
-        img_bgr = load_image('{}/qr-img.png'.format(base_path))
-        self.__test_qr(img_bgr,is_bgr=True)
-       
-     
+        img_bgr = load_image("{}/qr-img.png".format(base_path))
+        self.__test_qr(img_bgr, is_bgr=True)
+
     def test_qr_rgb(self):
-        img_bgr = load_image('{}/qr-img.png'.format(base_path))
+        img_bgr = load_image("{}/qr-img.png".format(base_path))
         img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB, img_bgr)
-        self.__test_qr(img_rgb,is_bgr=False)
-            
+        self.__test_qr(img_rgb, is_bgr=False)
+
     def test_qr_gray(self):
-        img_bgr = load_image('{}/qr-img.png'.format(base_path))
+        img_bgr = load_image("{}/qr-img.png".format(base_path))
         img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY, img_bgr)
         segmented = segmentation_auto_threshold(img_gray)
-        self.__test_qr(segmented,is_bgr=False)
-        
+        self.__test_qr(segmented, is_bgr=False)
+
         # this is obviously wrong as the format is not BGR but grayscale, but the conversion to BGR should take place and convert set the parameter correctly
-        img_bgr = load_image('{}/qr-img.png'.format(base_path))
+        img_bgr = load_image("{}/qr-img.png".format(base_path))
         img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY, img_bgr)
-        self.__test_qr(img_gray,is_bgr=True)
-        
+        self.__test_qr(img_gray, is_bgr=True)
+
     def test_qr_multiple_codes(self):
-        img_bgr = load_image('{}/qr-img-2.png'.format(base_path))
+        img_bgr = load_image("{}/qr-img-2.png".format(base_path))
         results = qr_detect_and_decode(img_bgr)
         self.assertEqual(len(results), 2)
         if results is not None:
             for result in results:
-                self.assertTrue(result in ['The meeting is at 6 p.m.', 'The meeting is at 6 p.m'])
+                self.assertTrue(
+                    result in ["The meeting is at 6 p.m.", "The meeting is at 6 p.m"]
+                )
         else:
             print("No QR codes found")
-        
-        
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     unittest.main()
--- tests/test_segmentation.py
+++ tests/test_segmentation.py
@@ -1,15 +1,21 @@
 import unittest
 from pathlib import Path
 
-from improutils.acquisition import *
-from improutils.visualisation import *
-from improutils.preprocessing import *
-from improutils.segmentation import *
+import cv2
+import numpy as np
+
+from improutils import (
+    load_image,
+    segmentation_one_threshold,
+    segmentation_two_thresholds,
+    to_3_channels,
+    to_gray,
+)
+
 
 class SegmentationCase(unittest.TestCase):
-
     def test_to_3_channels(self):
-        img_path = Path(__name__).parent.absolute() / 'tests' / 'img' / 'test-img.png'
+        img_path = Path(__name__).parent.absolute() / "tests" / "img" / "test-img.png"
         img_org = load_image(str(img_path))
         img_org = to_gray(img_org)
 
@@ -18,8 +24,8 @@ class SegmentationCase(unittest.TestCase):
 
     def test_segmentation_one_threshold(self):
         img = np.zeros((6, 6), np.uint8)
-        img[3:5, 2:4] = 255  #4 white pixels
-        img[2:3, 2:4] = 150 #2 gray pixels
+        img[3:5, 2:4] = 255  # 4 white pixels
+        img[2:3, 2:4] = 150  # 2 gray pixels
 
         img_bin1 = segmentation_one_threshold(img, 150)
         img_bin2 = segmentation_one_threshold(img, 100)
@@ -30,11 +36,10 @@ class SegmentationCase(unittest.TestCase):
         self.assertEqual(cv2.countNonZero(img_bin3), img.size)
 
     def test_segmentation_two_thresholds(self):
-
         img = np.zeros((6, 6), np.uint8)
-        img[3:5, 2:4] = 255  #4 white pixels
-        img[3:5, 4:5] = 30 #2 dark grey pixels
-        img[2:3, 2:4] = 150  #2 gray pixels
+        img[3:5, 2:4] = 255  # 4 white pixels
+        img[3:5, 4:5] = 30  # 2 dark grey pixels
+        img[2:3, 2:4] = 150  # 2 gray pixels
 
         img_bin1 = segmentation_two_thresholds(img, 0, 30)
         img_bin2 = segmentation_two_thresholds(img, 30, 150)
@@ -47,5 +52,5 @@ class SegmentationCase(unittest.TestCase):
         self.assertEqual(cv2.countNonZero(img_bin4), 32)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
