5fb5ae3642708c1f23e40bcdbd4020d728937e68
David Kramný <kramnda1@fit.cvut.cz>
2025-09-22 12:28:53 +0200

    Segmentation package linting and explicit imports

--- improutils/segmentation/__init__.py
+++ improutils/segmentation/__init__.py
@@ -1,1 +1,23 @@
-from .segmentation import *
+# segmentation functions
+from .segmentation import (
+    apply_mask,
+    segmentation_adaptive_threshold,
+    segmentation_auto_threshold,
+    segmentation_one_threshold,
+    segmentation_two_thresholds,
+    to_3_channels,
+    to_angle,
+    to_intensity,
+)
+
+# define public API
+__all__ = [
+    "apply_mask",
+    "to_intensity",
+    "to_angle",
+    "to_3_channels",
+    "segmentation_one_threshold",
+    "segmentation_auto_threshold",
+    "segmentation_two_thresholds",
+    "segmentation_adaptive_threshold",
+]
